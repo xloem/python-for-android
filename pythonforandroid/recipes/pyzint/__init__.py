c58dc3ee87c33b20c370e7babf0611acd12c7fb9
fuzzyTew <fuzzyTew@gmail.com>
2020-11-15 18:17:22 -0500

    reference a fork i made of pyzint for larger bar codes

--- pythonforandroid/recipes/pyzint/__init__.py
+++ pythonforandroid/recipes/pyzint/__init__.py
@@ -2,10 +2,9 @@ from os.path import join
 from pythonforandroid.recipe import CompiledComponentsPythonRecipe
 
 class PyZintRecipe(CompiledComponentsPythonRecipe):
-    version = '0.1.7'
-    url = 'https://files.pythonhosted.org/packages/39/42/b13d638dd53021c037544e404e18c785f4f4f9235afc7127efb95b9c5527/pyzint-0.1.7.tar.gz#sha256=f06e764780d157a1d319f83390ed67e366d215247678f189d93d24943ace1765'
-    md5sum = '8f3ad6dc515bbab2fb25b207397a792f'
-    blake2bsum = '84757348b619429633ba40117f1c68ddd9c932559afc664e8b156126642ce7562b20f18cbdc47ae158c00959be026ca18dc71974d785c434cc29948929799878'
+    version = '33821f2e184cbb4055f799fa01745ea7750543ba'
+    url = 'https://github.com/xloem/pyzint/archive/{version}.tar.gz#sha256=393fe78d9f1fad36530c403b665c4363438d5ff30635ccd89a86c65d2e1dfdd7'
+    md5sum = 'c4714892dd314bf276175d263e3ab4ce'
 
     depends = ['setuptools']
     
